3d8b24450699d3194b2749c53da202566e414f5c
miltonnunes52 <milton.nunes52@gmail.com>
2014-03-30 02:20:28 +0100

    scheduling added and other EndPoint changes

--- src/main/java/com/example/switchyard/CAMCoF/CommunicationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.switchyard.CAMCoF;
-
-public interface CommunicationService {
-	
-	JSONResponse ReceiveCommunication(JSONObject jsonObject);
-	JSONResponse SendResponse();
-
-}
--- src/main/java/com/example/switchyard/CAMCoF/ReadDataService.java
+++
@@ -1,8 +0,0 @@
-package com.example.switchyard.CAMCoF;
-
-public interface ReadDataService {
-	
-	public JSONResponse ReceiveCommunication(JSONObject jsonObject);
-	public JSONResponse SendResponse();
-
-}
